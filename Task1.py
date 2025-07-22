a285746baedc50437be5c1767a2a83a7c92b2c5a
chandanhiremathmath <153427427+chandanhiremathmath@users.noreply.github.com>
2025-07-22 11:12:02 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+a=float(input("Enter the 1st number: "))
+b=float(input("Enter the 2nd number: "))
+add=a+b
+print("Addition: ",add)
+sub=a-b
+print("Substraction: ",sub)
+mul=a*b
+print("Multiplication: ",mul)
+if b != 0:
+    div = a / b
+    print("Division:", div)
+else:
+    print("Division: Cannot divide by zero")
